43d417de619d3e4b5f77c6c525e33dad647ef117
sikami <widyasari.listya@gmail.com>
2021-02-08 13:48:43 +0000

    added testFileContainsContent()

--- src/main/Test/TestPasswordReader.java
+++ src/main/Test/TestPasswordReader.java
@@ -1,12 +1,23 @@
 import com.packagename.myapp.spring.PasswordReader;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class TestPasswordReader {
+
+    private PasswordReader passwordReader = new PasswordReader();
+
     @Test
     public void testFileExist() {
-        PasswordReader passwordReader = new PasswordReader();
+        assertTrue(passwordReader.isExist("config"));
+    }
+
+    @Test
+    public void testFileContainsContent() throws IOException {
         assertTrue(passwordReader.read("config"));
     }
+
+
 }
--- src/main/java/com/packagename/myapp/spring/PasswordReader.java
+++ src/main/java/com/packagename/myapp/spring/PasswordReader.java
@@ -1,14 +1,37 @@
 package com.packagename.myapp.spring;
 
-import java.io.File;
+import java.io.*;
 import java.nio.file.Path;
 
 public class PasswordReader {
 
+    private String apiKey;
+    private String url;
 
-    public boolean read(String config) {
+    public boolean isExist(String config) {
         File toRead = new File(String.valueOf(Path.of(config)));
         if(toRead.exists()){
+            return true;
+        }
+        return false;
+    }
+
+
+    public boolean read(String config) throws IOException {
+
+        if(isExist(config)) {
+            File toRead = new File(String.valueOf(Path.of(config)));
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(toRead));
+            String input = "";
+            String[] keys;
+            while ((input = bufferedReader.readLine()) != null) {
+                keys = input.split(" ");
+                if (input.contains("APIKEY")) {
+                    apiKey = keys[1];
+                } else {
+                    url = keys[1];
+                }
+            }
             return true;
         }
         return false;
