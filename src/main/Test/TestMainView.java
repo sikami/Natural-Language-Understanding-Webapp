a97bed77151955654050f1c18ed6693cbba30611
sikami <widyasari.listya@gmail.com>
2021-08-10 18:30:40 +0100

    downloaded geckodriver but wont work, so need to find out how to do it with chrome driver

--- src/main/Test/TestMainView.java
+++ src/main/Test/TestMainView.java
@@ -1,6 +1,26 @@
-
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class TestMainView {
+    private WebDriver webDriver;
+
+//    @BeforeAll
+    @Test
+    public void setup() {
+        System.setProperty("webdriver.gecko.driver", "/Users/listya/Documents/Lis/Lib/geckodriver 2");
+        webDriver = new FirefoxDriver();
+        String baseUrl = "http://demo.guru99.com/test/newtours/";
+        String expectedTitle = "Welcome: Mercury Tours";
+        String actualTitle = "";
+
+        webDriver.get(baseUrl);
+        actualTitle = webDriver.getTitle();
+        System.out.println(actualTitle);
+
+
+    }
 
 }
 
